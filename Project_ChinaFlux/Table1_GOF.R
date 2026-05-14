3e5fb5c42cc892602a0cbd10af8a58add358b5a8
Dongdong Kong <kongdd@users.noreply.github.com>
2026-05-14 11:32:48 +0800

    下一阶段目标开启，提升SM模拟

--- Project_ChinaFlux/Table1_GOF.R
+++ Project_ChinaFlux/Table1_GOF.R
@@ -1,3 +1,12 @@
+# %% GPP & ET
+gof_prev <- map(lst, \(l) l$gof$Flux) %>% melt_list("site")
+gof_optim <- map(lst, \(l) l$gof_opt$Flux) %>% melt_list("site")
+
+gof <- list("Original" = gof_prev, "Optimized" = gof_optim) %>% melt_list("type")
+dat <- melt(gof, c("type", "site", "var", "n_valid"), variable.name = "index") %>%
+  dcast(site + var + index ~ type, value.var = "value") %>%
+  merge(st, by = "site")
+
 Table1 <- function(VAR = "GPP", INDEX = "NSE") {
   mutate(dat, diff = Optimized - Original) %>%
     relocate(I, site, var, index, Original) %>%
@@ -12,5 +21,56 @@ tbl <- list(
   KGE = cbind(Table1("GPP", "KGE"), Table1("ET", "KGE")[, -(1:2)]),
   R2 = cbind(Table1("GPP", "R2"), Table1("ET", "R2")[, -(1:2)])
 )
+write_list2xlsx(tbl, "Table1_GOF_V4.xlsx", show = TRUE)
 
-write_list2xlsx(tbl, "Table1_GOF_V3.xlsx", show = TRUE)
+# %% SM
+# l <- lst[[1]]
+gof_prev <- map(lst, \(l) check_var(l$gof$SM)) %>% melt_list("site")
+gof_optim <- map(lst, \(l) check_var(l$gof_opt$SM)) %>% melt_list("site")
+gof <- list("Original" = gof_prev, "Optimized" = gof_optim) %>% melt_list("type")
+
+dat <- melt(gof, c("type", "site", "var", "n_valid"), variable.name = "index") %>%
+  dcast(site + var + index ~ type, value.var = "value") %>%
+  merge(st, by = "site")
+
+Table1 <- function(INDEX = "NSE") {
+  mutate(dat, diff = Optimized - Original) %>%
+    relocate(I, site, var, index, Original) %>%
+    arrange(I) %>%
+    subset(index == INDEX) %>%
+    select(-label, -index) %>%
+    dt_round(3)
+}
+
+tbl <- list(
+  NSE = Table1("NSE"),
+  KGE = Table1("KGE"),
+  R2 = Table1("R2")
+)
+# write_list2xlsx(tbl, "Table1_SM_GOF.xlsx", show = TRUE)
+
+# %% TS
+# l <- lst[[1]]
+gof_prev <- map(lst, \(l) check_var(l$gof$TS)) %>% melt_list("site")
+gof_optim <- map(lst, \(l) check_var(l$gof_opt$TS)) %>% melt_list("site")
+gof <- list("Original" = gof_prev, "Optimized" = gof_optim) %>% melt_list("type")
+
+dat <- melt(gof, c("type", "site", "var", "n_valid"), variable.name = "index") %>%
+  dcast(site + var + index ~ type, value.var = "value") %>%
+  merge(st, by = "site")
+
+Table1 <- function(INDEX) {
+  mutate(dat, diff = Optimized - Original) %>%
+    relocate(I, site, var, index, Original) %>%
+    arrange(I) %>%
+    subset(index == INDEX) %>%
+    select(-label, -index) %>%
+    dt_round(3)
+}
+
+tbl <- list(
+  NSE = Table1("NSE"),
+  KGE = Table1("KGE"),
+  R2 = Table1("R2")
+)
+write_list2xlsx(tbl, "Table1_TS_GOF.xlsx", show = TRUE)
